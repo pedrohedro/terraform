d2077cabc8214512e6f8cb27643f1381366a01fe
Pedro Eulogio Melotti Hedro <pehedro@latam.stefanini.com>
2023-02-22 22:46:25 -0300

    add gce module

---
+++ gce/main.tf
@@ -0,0 +1,28 @@
+provider "google" {
+  project = "seu-projeto-id"
+  region  = "us-central1"
+}
+
+resource "google_compute_instance" "instance" {
+  count        = 3
+  name         = "minha-instancia-${count.index + 1}"
+  machine_type = "n1-standard-1"
+  zone         = "us-central1-a"
+
+  boot_disk {
+    initialize_params {
+      image = "ubuntu-os-cloud/ubuntu-1804-lts"
+    }
+  }
+
+metadata = {
+  ssh-keys = "ubuntu:${file("~/.ssh/id_rsa.pub")}"
+}
+
+  network_interface {
+    network = "default"
+    access_config {
+      // Ephemeral IP
+    }
+  }
+}
